2311964c2c1e9e2806b9c8d427885c023c3c066d
Jorge <jorge.omar.medra@gmail.com>
2023-09-21 14:25:56 -0500

    Cambios a ser confirmados: 	modificados:     src/dump.h

--- src/dump.h
+++ src/dump.h
@@ -2,6 +2,18 @@
 #define DUMPL_H
 
 #include "test.h"
+
+
+/**
+ * @brief Use, on a commmond console of linux:
+ *  a) coredumpctl list    To identify the core file
+ *  b) coredumpctl [PID|EXE...]    To open del dumpfile to debug with gdb
+ * 
+ * onece the GDB is running, use the commando bt, or bt full to print the 
+ * stack and check the point where the program raise the error.
+ * 
+ */
+
 namespace jomt::test
 {
     class TestDump: public jomt::Test
